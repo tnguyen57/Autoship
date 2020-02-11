bbb219be8ee24cc37cf471cf970141507e8ce645
Thalia Nero <chrisvn00@gmail.com>
2020-02-10 23:12:03 -0500

    Set up basic Koa web server using npm

---
+++ index.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const Router = require('@koa/router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hello world';
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000);
