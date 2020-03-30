4236b5336607df0e31fbe50d2e34672ef3fb5588
tnguyen57 <terrynguyen57@gmail.com>
2020-03-30 14:03:14 -0500

    Added classes for ship and board data for the server.

---
+++ client/src/server/GameBoardData.js
@@ -0,0 +1,64 @@
+
+/**
+ * This class will store the game state of a players board on the server.
+ * This class will store current guesses and ship positions
+ */
+
+ import {GameShipData} from "GameShipData";
+ import {EnumPosition} from "../utilities";
+
+let GameBoardData = class{
+
+    constructor(shipArray){
+        /**
+         * @param {Array[GameShipData]} - Contains all of the ships to the board.
+         */
+        this._guesses = new Array();
+        this._remainingShips = shipArray;
+        this._sunkShip = new Array();
+    }
+
+    guess(x, y){
+        /**
+         * Make a guess on the board
+         * @param {Int} x - x coordinate of the guess
+         * @param {Int} y - y coordinate of the guess
+         * @modify - Make the ship as sunken/hit if the guess succeed
+         * @return {EnumPosition} - Returns whether the guess is a hit/miss/sink.
+         */
+
+        this._guesses.push(new Coordinate(x, y));
+
+        for (let i = 0; i < this._remainingShips.length; i++){
+            ship = this._remainingShips[i];
+            classification = ship.guess(x, y)
+            if (classification == EnumPosition.HIT){
+                return EnumPosition.HIT;
+
+            }
+            else if (classification == EnumPosition.SINK){
+                this._remainingShips.splice(i, 1);
+                this._sunkShip.push(ship);
+                return EnumPosition.SINK;
+            }
+        }
+        return EnumPosition.MISS;
+    }
+
+    get ShipCount(){
+        /**
+         * @return {Int} - Returns the number of ships left
+         */
+        return this._remainingShips.length;
+    }
+
+    get LastSunkShipID(){
+        /**
+         * @return {Int} - Returns the id of the last sunk ship. Return null if no ship sunk.
+         */
+        if (this._sunkShip.length == 0){
+            return null;
+        }
+        return this._sunkShip[this._sunkShip.length - 1].id;
+    }
+}
---
+++ client/src/server/GameShipData.js
@@ -0,0 +1,50 @@
+/**
+ * This class contains the information of a ship on the board.
+ * It will keep track of damages, it's position, and will allow for
+ * guesses on it.
+ */
+
+import {Coordinate} from "../utilities";
+import {EnumPosition} from "../utilities";
+
+let GameShipData = class{
+    constructor(coordinateArray, id = 0){
+        this._activePositions = coordinateArray;
+        this._hitPositions = new Array();
+        this._id = id;
+    }
+
+    guess(x, y){
+        /**
+         * Given an x and y position, check to see if the ship is hit.
+         * 
+         * @param {Int} x - The x coordinate of the guess
+         * @param {Int} y - The y coordinate of the guess
+         * @modify - On hit, mark the position as hit.
+         * @return {EnumPosition} - Returns if it is a hit/miss/skin
+         */
+        for (let i  = 0; i < this._activePositions.length; i++){
+            currentPosition = this._activePositions[i]
+            if (this._activePositions[i].x == x && this._activePositions[i].y == y){
+                this._hitPositions.push(currentPosition);
+                this._activePositions.splice(i, 1);
+                if (this.isSunk()){
+                    return EnumPosition.SINK;
+                }
+                return EnumPosition.HIT;
+            }
+        }
+        return EnumPosition.MISS;
+    }
+
+    isSunk(){
+        /**
+         * @return {Boolean} - Return if the Ship has sunk.
+         */
+        return this._activePositions.length == 0;
+    }
+
+    get id(){
+        return this._id;
+    }
+}
