0b686655b6e943733e35f1242d00489ba9bfbd95
tnguyen57 <terrynguyen57@gmail.com>
2020-04-05 15:08:19 -0400

    Finished Game after more testing. Added more functions.

--- server/Game.js
+++ server/Game.js
@@ -7,17 +7,29 @@
 const BoardData = require("./BoardData");
 const Guesses = require("./utilities");
 
+var gameCount = 0;
+
+function incrementCount(){
+    gameCount++;
+}
+
 
 let Game = class {
     /**
      * Construct a Basic Game using two ship arrays
      * @param {Array[ShipData]} shipArray1 
      * @param {Array[ShipData]} shipArray2 
+     * @param {Int} width;
+     * @param {Int} height;
      */
-    constructor(shipArray1, shipArray2){
-        this._board1 = new BoardData(shipArray1);
-        this._board2 = new BoardData(shipArray2);
-        this._firstPlayer = True; //Contains the 
+    constructor(shipArray1, shipArray2, width = 10, height = 10){
+        this._board1 = new BoardData(shipArray1, width, height);
+        this._board2 = new BoardData(shipArray2, width, height);
+        this._firstPlayer = true; //Contains the 
+        this._id = gameCount;
+        this._width = width;
+        this._height = height;
+        incrementCount();
     }
 
     /**
@@ -29,12 +41,12 @@ let Game = class {
      * @return {{firstPlayer:Boolean: , status:Guesses}} - Return an tuple containing the player and status. Player is true if first player. 
      */
     handleGuess(x, y){
-        firstPlayer = this._firstPlayer;
+        const firstPlayer = this._firstPlayer;
         this._firstPlayer = !(this._firstPlayer); //Swapping to next turn
         if (firstPlayer){
-            return {firstPlayer: firstPlayer, status:this._board1.guess(x, y)};
+            return {firstPlayer: firstPlayer, status:this._board2.guess(x, y)};
         }
-        return {firstPlayer: firstPlayer, status:this._board2.guess(x, y)};
+        return {firstPlayer: firstPlayer, status:this._board1.guess(x, y)};
     }
     
     /**
@@ -45,4 +57,46 @@ let Game = class {
     get isPlayerOne(){
         return this._firstPlayer;
     }
+
+    /**
+     * Returns the id for the game
+     * 
+     * @return {Int} - returns the game ID
+     */
+    get id(){
+        return this._id;
+    }
+
+    /**
+     * Returns the size of the board
+     * 
+     * @return {{width: Int, height: Int}} - returns the game ID
+     */
+    get size(){
+        return {width: this._width, height: this._height};
+    }
+
+    /**
+     * Returns the id of the last ship that sunk
+     * 
+     * @return {Int} - returns the game ID
+     */
+    get lastSunkID(){
+        if (this._firstPlayer){
+            return this._board1.lastSunkID;
+        }
+        return this._board2.lastSunkID;
+
+    }
+
+    /**
+     * Returns if the game is finished.
+     * 
+     * @return {Boolean} - returns true if the game is complete
+     */
+    get isFinish(){
+        return (this._board1.shipCount === 0 || this._board2.shipCount === 0);
+    }
 }
+
+module.exports = Game;
