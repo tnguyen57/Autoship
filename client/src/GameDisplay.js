2ddeb318425e99c567d5724a2a4276e785295a7d
Thalia Nero <chrisvn00@gmail.com>
2020-04-05 16:52:54 -0400

    Display two boards side-by-side

--- client/src/GameDisplay.js
+++ client/src/GameDisplay.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Square from './Square';
 
 /**
  * Displays a blank board using SVG. The board accepts child nodes,
@@ -8,9 +7,9 @@ import Square from './Square';
  *  - width (int): the display width in pixels. Also used for the height.
  */
 export default function GameDisplay(props) {
-  const { width } = props;
+  const { width, id } = props;
   return (
-    <svg width={width} height={width}>
+    <svg width={width} height={width} id={id}>
       {props.children}
     </svg>
   );
