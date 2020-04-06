a5029f94b91396828f6557c29eb87164ca351200
Chris Nero <chrisvn00@gmail.com>
2020-04-06 17:58:12 -0400

    Ship display on client

--- client/src/GameDisplay.js
+++ client/src/GameDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Ship from './Ship';
 import './GameDisplay.css';
 
 /**
@@ -6,12 +7,25 @@ import './GameDisplay.css';
  * which are rendered onto the display.
  * Props:
  *  - width (int): the display width in pixels. Also used for the height.
+ *  - ships ([record]): a list of ship types
  */
 export default function GameDisplay(props) {
-  const { width, id } = props;
+  const { width, ships, id } = props;
+  const shipComponents = ships.map(ship => (
+    <Ship
+      key={ship.name}
+      name={ship.name}
+      length{ship.length}
+    />
+  ));
   return (
-    <svg width={width} height={width} id={id} className="game-display">
-      {props.children}
-    </svg>
+    <div className="game-display">
+      <svg width={width} height={width} id={id}>
+        {props.children}
+      </svg>
+      <div>
+        {shipComponents}
+      </div>
+    </div>
   );
 }
