b014918b4b18722f40d2a9bcbf8b93c42d9b3870
Vivek Verghese <verghv5@gmail.com>
2020-04-06 01:08:30 -0400

    Added sample and test input files

--- AI/Board.cpp
+++ AI/Board.cpp
@@ -4,3 +4,18 @@
 #include "mtrand.h"
 #include "Board.h"
 
+Ship::Ship(unsigned int _size, std::string _name)
+{
+	size = _size;
+	health = _size;
+	sunk = false;
+	name = _name;
+}
+
+bool Ship::processHit()
+{
+	health--;
+	!health = sunk;
+	return sunk;
+}
+
