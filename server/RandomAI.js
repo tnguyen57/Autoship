ec50596382de64acc7a4e8a4feade05a1bd19d6b
tnguyen57 <terrynguyen57@gmail.com>
2020-04-06 17:09:23 -0400

    Added RandomAI file to ./server. It can randomly create a basic board and produce random guesses.

---
+++ server/RandomAI.js
@@ -0,0 +1,131 @@
+/**
+ * This class will act as a random AI. Creates a random board and make random guesses.
+ */
+const ShipData = require("./ShipData");
+
+const RandomAI = class{
+    constructor(width, height) {
+        this._possibleGuesses = this.getAllPositions(width, height);
+        this._width = width;
+        this._height = height;
+    }
+
+    /**
+     * Generate a random guess given the board state.
+     */
+    generateGuess(){
+        if (this._possibleGuesses.length === 0){
+            return null;
+        }
+        const index = this.getRndInteger(0, this._possibleGuesses.length);
+        const guess = this._possibleGuesses[index];
+        this._possibleGuesses.splice(index, 1);
+        return guess;
+    }
+
+    /**
+     * Generates an array of all possible positions given board size
+     * 
+     * @returns {Array[{x: Int, y: Int}]} - Returns an array of all possible positons on the board
+     */
+    getAllPositions(width, height) {
+        let positions = [];
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++){
+                positions.push({x: x, y: y});
+            }
+        }
+        return positions;
+    }
+
+    /**
+     * Generates and return a random integer between 
+     * min (inclusive and max (exlucisve)
+     * 
+     * @param {Int} min - The lower inclusive bound of the  random integer generator
+     * @param {Int} max - The upper exclusive bound of the  random integer generator
+     * @return {Int} - The randomly generated integer between min (inclusive) and max (exclusive)
+     */
+    getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+    }
+
+    /**
+     * Helper function to check used coordinates to make sure generated ship is valid
+     */
+    checkShip(ship, usedCoordinates){
+        for(let i = 0; i < ship.length; i++){
+            const coord = ship[i];
+            for(let j = 0; j < usedCoordinates.length; j++){
+                if (coord.x >= this._width || coord.y >= this._height){
+                    return false;
+        
+                }
+                if (coord.x === usedCoordinates[j].x && coord.y === usedCoordinates[j].y){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * Generate an array of random ships that follows the basic rules of Battleship.
+     */
+    getBasicBoardShips() {
+        let shipArray = [];
+        let usedCoordinates = [];
+        let firstTime = true;
+        let id = 1;
+        for (let i = 2; i < 6; i++) {
+            let ship = this.getRandomShip(i, usedCoordinates);
+            if (ship === null){
+                return null;
+            }
+            shipArray.push(new ShipData(ship, id++));
+            if(i === 3 && firstTime) {
+                i--;
+                firstTime = false;
+            }
+        }
+        return shipArray;
+
+    }
+
+    /**
+     * Generate random ship
+     */
+    getRandomShip(size, usedCoordinates){
+        for (let i = 0; i < 20000; i++){
+            const index = this.getRndInteger(0, this._possibleGuesses.length);
+            const start = this._possibleGuesses[index];
+            const orientation = this.getRndInteger(0, 2);
+            let ship = this.generateShip(size, start, orientation);
+            if (this.checkShip(ship, usedCoordinates)){
+                for (let j = 0; j < size; j++){
+                    usedCoordinates.push(ship[j]);
+                }
+                return ship;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Create an array of coordinates corresponding to a ship.
+     */
+    generateShip(size, start, orientation){
+        let ship = [];
+        for (let i = 0; i < size; i++){
+            if (orientation === 1){
+                ship.push({x: start.x, y: start.y + i});
+            }
+            else{
+                ship.push({x: start.x + i, y: start.y});
+            }
+        }
+        return ship;
+    }
+}
+
+module.exports = RandomAI;
