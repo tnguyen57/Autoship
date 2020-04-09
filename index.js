5ab3beb8799b68fb5899201ed8b7eec11b7a7289
tnguyen57 <terrynguyen57@gmail.com>
2020-04-09 16:05:23 -0500

    Added code skeletons for functions to handle ctx requests.

--- index.js
+++ index.js
@@ -12,4 +12,63 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
+/**
+ * Start a game against an AI
+ * @param {*} ctx 
+ * @param {*} next 
+ */
+async function startGameAI(ctx, next) {
+  // parse client request using ctx
+  await next();
+  // build response using ctx
+}
+
+/**
+ * Start a game against another person
+ * @param {*} ctx 
+ * @param {*} next 
+ */
+async function startGame(ctx, next) {
+  // parse client request using ctx
+  await next();
+  // build response using ctx
+}
+
+
+/**
+ * Start a game against an AI
+ * @param {*} ctx 
+ * @param {*} next 
+ */
+async function startGameAI(ctx, next) {
+  // parse client request using ctx
+  await next();
+  // build response using ctx
+}
+
+/**
+ * Handles the guess for player one
+ * @param {*} ctx 
+ * @param {*} next 
+ */
+async function handleGuess1(ctx, next) {
+  // parse client request using ctx
+  await next();
+  // build response using ctx
+}
+
+/**
+ * Handles the guess for player two
+ * @param {*} ctx 
+ * @param {*} next 
+ */
+async function handleGuess2(ctx, next) {
+  // parse client request using ctx
+  await next();
+  // build response using ctx
+}
+
+
+
+
 app.listen(3001);
