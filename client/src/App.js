836c5bd97a300837ab321a7db2d4e8d641f371ad
Chris Nero <chrisvn00@gmail.com>
2020-04-15 19:11:31 -0400

    Remove unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import MainMenu from "./MainMenu";
 import RenderBoard from "./RenderBoard";
 
